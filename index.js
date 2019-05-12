e32f7bf3df1995bf371e92c3d1e9f29cfcaedd38
杨明山 <y.mingshan3@gmail.com>
2019-05-12 20:58:10 +0800

    Use options

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ const path = require('path');
 const fsPromises = require('fs').promises;
 const bodyParser = require('body-parser');
 
-module.exports = function mockerDataWebpack(
-  prefix = '/api',
-  dir = path.join(process.cwd(), 'mock')
-) {
+module.exports = function mockerDataWebpack(options = {}) {
+  options.prefix = options.prefix || '/api';
+  options.path = options.path || 'mock';
+
   return function mockerDataMiddleware(app) {
     app.use(bodyParser.json());
 
+    const { prefix } = options;
     app.all(`${prefix}/*`, async (req, res) => {
       res.set('Cache-Control', 'no-store');
 
       let apis = {};
       try {
+        let dir = options.path;
+        if (!path.isAbsolute(dir)) {
+          dir = path.join(process.cwd(), dir);
+        }
         const files = await fsPromises.readdir(dir);
         files.forEach(file => {
           const filePath = path.join(dir, file);
@@ -26,8 +31,8 @@ module.exports = function mockerDataWebpack(
         return res.sendStatus(404);
       }
 
-      const path = req.path.replace(prefix, '');
-      const api = apis[path];
+      const pathname = req.path.replace(prefix, '');
+      const api = apis[pathname];
 
       if (api === undefined) {
         return res.sendStatus(404);
